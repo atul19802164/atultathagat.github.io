153ecb0161272b17f7c4ef0ac8e498d6e8d6d3ee
atul19802164 <44753404+atul19802164@users.noreply.github.com>
2018-11-05 00:38:18 +0530

    first website upload

---
+++ scroll.js
@@ -0,0 +1,8 @@
+
+   // console.log(get_header);
+    $(".mymenu .navbar-nav  li a").click(function(e){
+         e.preventDefault();
+        var a = $(this).attr("href");
+        console.log(a);
+       
+    });
